69c1a4982d954282b9960ae6c47319cfac5ec767
GonWick <gonxza@gmail.com>
2021-01-18 13:43:14 +0100

    más + Retoques

--- Ejercicio_Bucle_Do_While_18_01_2021/Form1.Designer.vb
+++ Ejercicio_Bucle_Do_While_18_01_2021/Form1.Designer.vb
@@ -48,9 +48,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(12, 19)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(160, 15)
+        Me.Label1.Size = New System.Drawing.Size(154, 15)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Introducca Número a Repetir"
+        Me.Label1.Text = "Introduzca número a repetir"
         '
         'TextBox1
         '
@@ -75,7 +75,7 @@ Partial Class Form1
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(91, 15)
         Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Número >= 500"
+        Me.Label3.Text = "Número <= 500"
         '
         'TextBox2
         '
